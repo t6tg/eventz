fb3aa4a837374b1b74c00dbc7d4f3fd0415d81eb
m.mjames <work.t6tg@gmail.com>
2025-03-08 16:32:16 +0700

    faat: registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,26 @@ class RegistrationsController < ApplicationController
     @event = Event.find(event_id)
     @registrations = @event.registrations
   end
+
+
+  def new
+    @event = Event.find(params[:event_id])
+    @registrations = Registration.new
+  end
+
+  def create
+    # fail
+    @event = Event.find(params[:event_id])
+    @registrations = @event.registrations.new(registration_params)
+    if @registrations.save
+      redirect_to event_registrations_url(@event), notice: "Thanks for registring!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def registration_params
+    params.require(:registration).permit(:name, :email, :how_heard)
+  end
 end
