3a37bc71fa310f1111dfb46088f2920569b9f383
m.mjames <work.t6tg@gmail.com>
2025-03-08 19:44:48 +0700

    feat: sign-in and sign-out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :events do
     resources :registrations
   end
+
+  resource :session, only: [ :new, :create, :destroy ]
+
   resources :users
   get "signup" => "users#new"
 end
